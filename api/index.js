803274b4f59c24ced6e82b5f6261242cff8e4570
Sachin Suryawanshi <sachin.suryawanshi@entertainmenttechnologists.com>
2025-06-06 11:08:44 +0530

    build issues

---
+++ api/index.js
@@ -0,0 +1,8 @@
+// api/index.js
+import app from '../app.js';
+import { createServer } from 'http';
+
+export default async function handler(req, res) {
+  const server = createServer(app);
+  server.emit('request', req, res);
+}
