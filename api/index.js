0a928d12142eb6d13e367242b9b5da4d11e55ddd
Sachin Suryawanshi <sachin.suryawanshi@entertainmenttechnologists.com>
2025-06-06 11:55:23 +0530

    fixed cors error

--- api/index.js
+++ api/index.js
@@ -3,6 +3,5 @@ import app from '../app.js';
 import { createServer } from 'http';
 
 export default async function handler(req, res) {
-  const server = createServer(app);
-  server.emit('request', req, res);
+    await app(req, res);
 }
--- app.js
+++ app.js
@@ -59,6 +59,7 @@ const corsOptions = {
 };
 
 app.use(cors(corsOptions));
+app.options('*', cors(corsOptions));
 app.use(compression());
 app.use(express.json());
 app.use(cookieParser());
