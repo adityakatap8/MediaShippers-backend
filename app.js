f8e043dc28154c63517cb23e9f29b51fccc1bfef
Sachin Suryawanshi <sachin.suryawanshi@entertainmenttechnologists.com>
2025-06-17 18:13:07 +0530

    added limit of file size

--- app.js
+++ app.js
@@ -62,7 +62,8 @@ const corsOptions = {
 app.use(cors(corsOptions));
 app.options('*', cors(corsOptions));
 app.use(compression());
-app.use(express.json());
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 app.use(cookieParser());
 
 // Logger
